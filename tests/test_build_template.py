f975e7224880275490b0e20f2d1c4e9a0ce2f5ce
KangWon LEE <kangwon.lee@kpu.ac.kr>
2020-08-24 18:07:12 +0900

    test whether the parser can identify arguments

--- tests/test_build_template.py
+++ tests/test_build_template.py
@@ -14,10 +14,35 @@ sys.path.insert(
 import build_template as bt
 
 
-def test_get_arg_parser_type_check():
-    parser = bt.get_arg_parser()
+@pytest.fixture
+def parser() -> argparse.ArgumentParser:
+    return bt.get_arg_parser()
+
+
+def test_get_arg_parser_type_check(parser):
 
     assert isinstance(parser, argparse.ArgumentParser)
+
+
+def test_get_arg_parser_arguments(parser):
+    src_url = 'https://github.com/kangwonlee/build_template'
+    ref = 'develop'
+    new_folder = 'build_here'
+    dest_url = 'https://github.com/kangwonlee/new_template'
+
+    parsed = parser.parse_args(
+        [
+            '-s', src_url,
+            '-r', ref,
+            '-n', new_folder,
+            '-d', dest_url,
+        ]
+    )
+
+    assert parsed.src_repo_url == src_url
+    assert parsed.reference == ref
+    assert parsed.new_empty_folder == new_folder
+    assert parsed.dest_repo_url == dest_url
 
 
 if "__main__" == __name__:
